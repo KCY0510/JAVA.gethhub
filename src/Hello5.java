286de7a9d62bc9d5c95cf05a6fde55f28a4acd55
DESKTOP-S17TTPF\USER <cocjswo8670@naver.com>
2023-03-09 12:14:32 +0900

    no message

--- src/Hello5.java
+++ src/Hello5.java
@@ -4,7 +4,7 @@ public class Hello5 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello");
-		System.out.println("Hello1");
+		System.out.println("Hello2");
 		System.out.println("Hello world");
 		System.out.println("good");
 		
