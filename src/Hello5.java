47b4b8fd331ca7b3fc9ec8a52cba0300355f150e
DESKTOP-S17TTPF\USER <cocjswo8670@naver.com>
2023-03-09 12:18:57 +0900

    commit change

--- src/Hello5.java
+++ src/Hello5.java
@@ -6,8 +6,8 @@ public class Hello5 {
 		System.out.println("Hello");
 		System.out.println("hi");
 		System.out.println("Hello world");
-		System.out.println("good");
-		
+		System.out.println("gef");
+
 		;
 
 	}
